914fd2a68ee2677b72604e36c5536f4fc4302c4a
jessica-m-r <jessiq51@gmail.com>
2026-04-14 21:58:31 -0400

    feat: redimensión de la nave dependiendo del tamaño de la pantalla

--- modules/nave/nave.js
+++ modules/nave/nave.js
@@ -1,52 +1,86 @@
-const canvas = document.getElementById("miCanvas");
-const ctx = canvas.getContext("2d");
+class Nave {
+    constructor(canvasId) {
+        this.canvas = document.getElementById(canvasId);
+        this.ctx = this.canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.moveTo(200, 80);  
-ctx.lineTo(250, 200); 
-ctx.lineTo(200, 180);
-ctx.lineTo(150, 200);
-ctx.closePath();       
-ctx.strokeStyle = "white";
-ctx.stroke();
+        this.ajustarPantalla();
 
-ctx.beginPath();
-ctx.moveTo(160, 170);
-ctx.lineTo(140, 220);
-ctx.lineTo(130, 190);
-ctx.closePath();
-ctx.strokeStyle = "white";
-ctx.stroke();
+        window.addEventListener("resize", () => {
+            this.ajustarPantalla();
+            this.dibujar();
+        });
+    }
 
-ctx.beginPath();
-ctx.moveTo(240, 170);
-ctx.lineTo(270, 190);
-ctx.lineTo(260, 220);
-ctx.closePath();
-ctx.strokeStyle = "white";
-ctx.stroke();
+    ajustarPantalla() {
+        this.canvas.width  = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+        this.cx = this.canvas.width  / 2;
+        this.cy = this.canvas.height / 2;
+    }
 
-ctx.beginPath();
-ctx.moveTo(160, 205);
-ctx.lineTo(180, 195);
-ctx.lineTo(170, 230);
-ctx.closePath();
-ctx.strokeStyle = "white";
-ctx.stroke();
+    dibujar() {
+        const ctx = this.ctx;
+        const cx  = this.cx;
+        const cy  = this.cy;
+        const s   = 0.5; 
 
-ctx.beginPath();
-ctx.moveTo(185, 195);
-ctx.lineTo(200, 190);
-ctx.lineTo(215, 195);
-ctx.lineTo(200, 230);
-ctx.closePath();
-ctx.strokeStyle = "white";
-ctx.stroke();
+        ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        ctx.strokeStyle = "white";
 
-ctx.beginPath();
-ctx.moveTo(240, 205);
-ctx.lineTo(220, 195);
-ctx.lineTo(230, 230);
-ctx.closePath();
-ctx.strokeStyle = "white";
-ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(cx,           cy - 60*s);
+        ctx.lineTo(cx + 50*s,    cy + 60*s);
+        ctx.lineTo(cx,           cy + 40*s);
+        ctx.lineTo(cx - 50*s,    cy + 60*s);
+        ctx.closePath();
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(cx - 40*s,    cy + 20*s);
+        ctx.lineTo(cx - 70*s,    cy + 90*s);
+        ctx.lineTo(cx - 75*s,    cy + 50*s);
+        ctx.closePath();
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(cx + 40*s,    cy + 20*s);
+        ctx.lineTo(cx + 75*s,    cy + 50*s);
+        ctx.lineTo(cx + 70*s,    cy + 90*s);
+        ctx.closePath();
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(cx - 45*s,    cy + 65*s);
+        ctx.lineTo(cx - 20*s,    cy + 55*s);
+        ctx.lineTo(cx - 35*s,    cy + 90*s);
+        ctx.closePath();
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(cx - 15*s,    cy + 55*s);
+        ctx.lineTo(cx,           cy + 47*s);
+        ctx.lineTo(cx + 15*s,    cy + 55*s);
+        ctx.lineTo(cx,           cy + 90*s);
+        ctx.closePath();
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(cx + 45*s,    cy + 65*s);
+        ctx.lineTo(cx + 20*s,    cy + 55*s);
+        ctx.lineTo(cx + 35*s,    cy + 90*s);
+        ctx.closePath();
+        ctx.stroke();
+
+        ctx.strokeStyle = "red";
+        ctx.beginPath();
+        ctx.moveTo(cx - 20*s, cy - 10*s);
+        ctx.lineTo(cx + 20*s, cy - 10*s);
+        ctx.lineTo(cx + 20*s, cy + 40*s);
+        ctx.lineTo(cx - 20*s, cy + 40*s);
+        ctx.closePath();
+        ctx.stroke();
+    }
+}
+
+const nave =  new Nave("miCanvas");
+nave.dibujar();
